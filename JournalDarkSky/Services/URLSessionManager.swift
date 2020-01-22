287b9aa05045a4d9e908e080fd9ee9dfe067077f
IamUzo <iamuzoagu@gmail.com>
2020-01-22 10:03:32 -0700

    first commit

---
+++ JournalDarkSky/Services/URLSessionManager.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionManager.swift
+//  JournalDarkSky
+//
+//  Created by Jared Warren on 1/20/20.
+//  Copyright © 2020 Warren. All rights reserved.
+//
+
+import Foundation
+
+class URLSessionManager {
+    
+    static func fetchData(for url: URL, completion: @escaping (Result<Data, NetworkError>) -> Void) {
+        
+        // TODO: Fetch Data from URL
+    }
+}
